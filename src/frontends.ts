a19de07fdc2865fa50fa2d6d2ee9ba0d0591c8ea
Alexandre BIGUET <alexandre.biguet@gmail.com>
2022-11-19 16:21:59 +0100

    feat(frontend): add markmap markdown frontend

--- src/frontends.ts
+++ src/frontends.ts
@@ -1,3 +1,5 @@
+import { createWriteStream } from "fs";
+import { sep } from "path";
 import { AdjacencyList } from "./graph";
 
 export class JsonNode {
@@ -13,7 +15,7 @@ export class JsonNode {
 }
 
 export function graphToJSON(graph: AdjacencyList): JsonNode {
-  const size = graph.adjacencyList.size;
+  const size = graph.adjacencyList.size; // TODO unused
   const keys = Array.from(graph.adjacencyList.keys());
 
   let treeBuilder = (keyIndex: number, parent: string | null) => {
@@ -40,4 +42,52 @@ export function graphToJSON(graph: AdjacencyList): JsonNode {
   let rootNode: JsonNode = treeBuilder(0, null);
 
   return rootNode;
+}
+
+export function graphToMarkMap(
+  graph: AdjacencyList,
+  stream: NodeJS.WritableStream
+) {
+  const separator = "  ";
+  const firstLevelAnchor = "#";
+  const otherAnchor = "-";
+  const endLine = "\n";
+  const space = " ";
+
+  const keys = Array.from(graph.adjacencyList.keys());
+
+  let treeBuilder = (depth: number, keyIndex: number) => {
+    if (depth === 0) {
+      stream.write(firstLevelAnchor);
+    } else {
+      stream.write(separator.repeat(depth - 1));
+      stream.write(otherAnchor);
+    }
+    const name = keys[keyIndex];
+    stream.write(space + name + endLine);
+
+    if (depth === 0) {
+      stream.write(endLine);
+    }
+
+    const children = graph.adjacencyList.get(name);
+
+    children?.map((elem) => {
+      const index = keys.indexOf(elem);
+      treeBuilder(depth + 1, index);
+    });
+  };
+
+  writeMarkmapHeader(stream);
+  treeBuilder(0, 0);
+}
+
+function writeMarkmapHeader(stream: NodeJS.WritableStream) {
+  stream.write(
+    `---
+markmap:
+  colorFreezeLevel: 2
+  maxWidth: 150
+--- \n\n`
+  );
 }
